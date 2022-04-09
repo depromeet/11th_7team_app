70c7b1499942a4a341f56908e68e1f596b6a238c
positiveko <edie.spark1er@gmail.com>
2022-04-09 12:46:43 +0900

    env: typescirpt 설정 위한 dev 패키지 추가 및 prettier 설정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  semi: true,
+  printWidth: 100,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
+  tabWidth: 2,
+  arrowParens: 'avoid',
 };
