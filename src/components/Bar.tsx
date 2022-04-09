87ee05681b40526abb0dcda3621ad5cf522ac968
positiveko <edie.spark1er@gmail.com>
2022-04-10 02:30:36 +0900

    env: eslint rule 추가 및 절대경로 설정

---
+++ src/components/Bar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function Bar() {
+  return (
+    <SafeAreaView>
+      <Text>Bar.tsx</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Bar;
