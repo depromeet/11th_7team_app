ce25bb67cb0d5b7fa15582eb729bc6bf979fb464
positiveko <edie.spark1er@gmail.com>
2022-05-24 01:48:33 +0900

    feat: react native navigation 및 의존 라이브러리 추가

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import LottieView from 'lottie-react-native';
 import { SafeAreaView } from 'react-native';
 
 import theme from '~/styles/theme';
 
 function Splash() {
-  return <SafeAreaView style={{ flex: 1, backgroundColor: theme.color.background }}>
-
-    
-  </SafeAreaView>;
+  const onSplashFinish = () => {
+    console.log('ye');
+  };
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.color.background }}>
+      <LottieView
+        source={require('../../assets/ygt-splash.json')}
+        autoPlay
+        loop={false}
+        onAnimationFinish={onSplashFinish}
+      />
+    </SafeAreaView>
+  );
 }
 
 export default Splash;
