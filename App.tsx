c508c6cf843283ad2228a7391bb58e9cbe1cc9f4
positiveko <edie.spark1er@gmail.com>
2022-05-24 01:41:26 +0900

    env: lottie react native library 및 lottie 추가

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
 
+import theme from '~/styles/theme';
+
 const App = () => {
   return (
     <>
       <StatusBar barStyle="default" />
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#D6DBDC' }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: theme.color.background }}>
         <WebView source={{ uri: 'https://app.ygtang.kr' }} />
       </SafeAreaView>
     </>
---
+++ src/screens/Splash.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import theme from '~/styles/theme';
+
+function Splash() {
+  return <SafeAreaView style={{ flex: 1, backgroundColor: theme.color.background }}>
+
+    
+  </SafeAreaView>;
+}
+
+export default Splash;
