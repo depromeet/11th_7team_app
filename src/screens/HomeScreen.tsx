f1da002e219d14d5119148a06e196bfd4c839961
Positive Ko <edie.spark1er@gmail.com>
2022-05-25 21:25:53 +0900

    fix: 앱 내 링크 썸네일 클릭 시 새 창으로 열리지 않는 에러 해결 (#26)

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { Linking, SafeAreaView, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 import theme from '~/styles/theme';
 
 export default function HomeScreen() {
+  const uri = 'https://app.ygtang.kr/';
   return (
     <>
       <StatusBar barStyle="default" />
       <SafeAreaView style={{ flex: 1, backgroundColor: theme.color.background }}>
-        <WebView source={{ uri: 'https://app.ygtang.kr' }} />
+        <WebView
+          source={{ uri }}
+          onNavigationStateChange={event => {
+            if (!event.url.includes(uri)) {
+              Linking.openURL(event.url);
+              return false;
+            }
+            return true;
+          }}
+        />
       </SafeAreaView>
     </>
   );
