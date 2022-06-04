d3c5c2e47ad8350920549e89e21061dc146b174c
positiveko <edie.spark1er@gmail.com>
2022-06-04 10:27:06 +0900

    design: launch screen background, tint 색상 변경 및 ygt 문구 삭제

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Linking, Platform, SafeAreaView, StatusBar } from 'react-native';
+import { Linking, Platform, StatusBar, View } from 'react-native';
 import { WebView, WebViewNavigation } from 'react-native-webview';
 
 import theme from '~/styles/theme';
@@ -8,8 +8,7 @@ const uri = 'https://app.ygtang.kr/';
 
 export default function HomeScreen() {
   const handleExternalLinks = (event: WebViewNavigation) => {
-    const isExternalLink =
-      Platform.OS === 'ios' ? event.navigationType === 'click' : true;
+    const isExternalLink = Platform.OS === 'ios' ? event.navigationType === 'click' : true;
     if (isExternalLink) {
       Linking.canOpenURL(event.url).then(supported => {
         if (supported) {
@@ -19,12 +18,12 @@ export default function HomeScreen() {
       return false;
     }
     return true;
-  }
+  };
 
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor={theme.color.background} />
-      <SafeAreaView style={{ flex: 1, backgroundColor: theme.color.background }}>
+      <View style={{ flex: 1, backgroundColor: theme.color.background }}>
         <WebView
           source={{ uri }}
           bounces={false}
@@ -32,7 +31,7 @@ export default function HomeScreen() {
           onNavigationStateChange={handleExternalLinks}
           onShouldStartLoadWithRequest={handleExternalLinks}
         />
-      </SafeAreaView>
+      </View>
     </>
   );
 }
