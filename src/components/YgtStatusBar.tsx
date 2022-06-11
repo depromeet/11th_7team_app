495788945ff50a9328978da071456347675e91cd
도다 <ddark.kr@gmail.com>
2022-06-11 15:29:55 +0900

    안드로이드 여백 이슈 해결 (#76)

--- src/components/YgtStatusBar.tsx
+++ src/components/YgtStatusBar.tsx
@@ -5,7 +5,5 @@ export function YgtStatusBar() {
   if (Platform.OS === 'ios') {
     return <StatusBar hidden={true} />;
   }
-  return (
-    <StatusBar translucent backgroundColor="transparent" barStyle={'dark-content'} hidden={false} />
-  );
+  return <StatusBar backgroundColor="#D6DBDC" barStyle={'dark-content'} />;
 }
