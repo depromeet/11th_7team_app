fae6e91d59f00ca66ec07785b0d70c6595c3826d
정대윤 <sensecodevalue@gmail.com>
2022-06-22 02:13:22 +0900

    fix: logout할 경우, accessToken empty string처리

--- src/hooks/useShareWebToken.ts
+++ src/hooks/useShareWebToken.ts
@@ -18,7 +18,16 @@ export function useShareWebToken() {
     if (Platform.OS !== 'ios') return `(function() {})();`;
     try {
       const refreshToken = await SharedGroupPreferences.getItem(SYNC_YGT_RT, APP_GROUP_KEY);
-      return `(function() {window.localStorage.setItem('${localStorageUserTokenKeys.refreshToken}','${refreshToken}');})();`;
+      return `(function() {
+          window.localStorage.setItem('${
+            localStorageUserTokenKeys.refreshToken
+          }','${refreshToken}');
+          if(${!refreshToken}) {
+            window.localStorage.setItem('${
+              localStorageUserTokenKeys.accessToken
+            }','${refreshToken}');
+          }
+        })();`;
     } catch (error) {
       if (error === 0) console.error('App Group이 없습니다.');
       return `(function() {})();`;
