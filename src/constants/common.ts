daec65b9a1e2a1cad054ccb1ad2f13e33ff766fc
doda <ddark.kr@gmail.com>
2022-06-29 15:55:36 +0900

    feat: 외부 페이지로 나가면 공유 해제되게

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,5 +1,10 @@
 export const SYNC_YGT_RT = 'SYNC_YGT_RT';
-export const BASE_URI = 'https://app.ygtang.kr/';
+export const BASE_URI = 'https://issue-465.ygt.pages.dev/';
 export const SHARE_EXTENTION_MESSAGE_TYPE = 'YgtangAppShareData';
 export const SHARE_WEB_MESSAGE_STATE = 'YgtangWebShareState';
 export const APP_GROUP_KEY = 'group.org.reactjs.native.example.ygt-share';
+
+export const WEBVIEW_MESSAGE_TYPE = {
+  CreatedInspiration: 'CreatedInspiration',
+  ClosedInspiration: 'ClosedInspiration',
+};
