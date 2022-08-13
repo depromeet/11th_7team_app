054e5fd26a0803d34242219d3637923a3f86b2f9
정대윤 <sensecodevalue@gmail.com>
2022-08-13 18:25:35 +0900

    env: ios firebase setup

--- ios/ygt/AppDelegate.mm
+++ ios/ygt/AppDelegate.mm
@@ -7,6 +7,8 @@
 #import <React/RCTAppSetupUtils.h>
 #import <RNShareMenu/ShareMenuManager.h>
 
+#import "Firebase.h"
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -30,6 +32,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
