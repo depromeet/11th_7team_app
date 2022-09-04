95891249a7bafd68d5c4c39f28a234d7ee2c6e79
positiveko <edie.spark1er@gmail.com>
2022-09-04 15:30:49 +0900

    chore: 임시 base uri 설정

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,6 +1,6 @@
 export const SYNC_YGT_RT = 'SYNC_YGT_RT';
-// export const BASE_URI = 'https://app.ygtang.kr/';
-export const BASE_URI = 'http://localhost:3000';
+// TODO 임시 테스트용
+export const BASE_URI = 'https://issue-459.ygt.pages.dev';
 export const AWS_S3_IMG_BUCKET_URI = 'https://s3.ap-northeast-2.amazonaws.com';
 export const SHARE_WEB_MESSAGE_STATE = 'YgtangWebShareState';
 export const APP_GROUP_KEY = 'group.org.reactjs.native.example.ygt-share';
